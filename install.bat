f263382a50d76ce89886bc4d79f055c280b0c386
daniel1zzz <daniel.developer.make@gmail.com>
2023-08-06 18:31:04 -0400

    Add comments with LICENSE in files install.bat and winape_install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,27 @@
 @echo off
 
+:: MIT License
+::
+:: Copyright (c) 2023 Daniel Andino Camacho
+::
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files (the "Software"), to deal
+:: in the Software without restriction, including without limitation the rights
+:: to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+:: copies of the Software, and to permit persons to whom the Software is
+:: furnished to do so, subject to the following conditions:
+::
+:: The above copyright notice and this permission notice shall be included in all
+:: copies or substantial portions of the Software.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+:: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+:: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+:: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+:: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+:: SOFTWARE.
+
 :: Global config
 
 set WINAPE_LOAD_DIR=winape_load
@@ -21,8 +43,8 @@ echo.
 echo This script allows you to install the winape_load scripts to the system.
 echo.
 echo  - Created by Daniel Andino Camacho
-echo  - Version 1.0
-echo  - Date 8/6/2023
+echo  - Version 0.1
+echo  - Date 8/7/2023
 echo.
 
 :: Verify that it is not already installed
